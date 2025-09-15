e0c53b83abcfd508fd4cea650c43cb96cafa2720
rmarpg <juneschema27@gmail.com>
2025-09-15 21:36:04 +0800

    feat: first commit

---
+++ src/router/index.ts
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '@/views/Login.vue'
+import Register from '@/views/Register.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register,
+    },
+  ],
+})
+
+export default router
---
+++ src/views/Login.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="flex min-h-screen flex-col items-center bg-blue-800 sm:justify-center">
+    <div class="mx-auto mt-6 w-full max-w-lg sm:mt-0">
+      <div class="px-4 py-6 sm:rounded-lg sm:bg-white sm:shadow-lg">
+        <form @submit="onSubmit" method="post">
+          <div>
+            <label for="email" class="sr-only">Emaiil address</label>
+            <Input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email address"
+              autocomplete="username"
+              required
+            />
+          </div>
+
+          <div class="mt-4">
+            <label for="password" class="sr-only">Password</label>
+            <Input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Password"
+              autocomplete="current-password"
+              required
+            />
+          </div>
+
+          <Button variant="default" class="mt-4 w-full">Login</Button>
+        </form>
+      </div>
+      <div class="mx-4 border-t border-gray-300"></div>
+      <div class="mx-auto mt-2 text-center sm:mt-4">
+        <RouterLink to="/register" class="text-center font-medium text-neutral-100">
+          Create an account
+        </RouterLink>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+
+const onSubmit = (event: Event) => {
+  event.preventDefault()
+  console.log('hello world')
+}
+</script>
