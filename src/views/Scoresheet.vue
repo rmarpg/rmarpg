d623c771d60540fd0b5b2bb892b4eb407a36e7b4
rmarpg <juneschema27@gmail.com>
2025-10-12 18:37:18 +0800

    feat: new scoresheet

--- src/views/Scoresheet.vue
+++ src/views/Scoresheet.vue
@@ -24,57 +24,123 @@ const selectedGrade = ref('grade_1')
       </Select>
     </header>
 
-    <table class="mt-6 w-full border-collapse">
-      <thead class="bg-blue-600">
-        <tr>
-          <th class="border border-gray-300 px-4 py-2 text-left font-semibold text-white">S/N</th>
-          <th class="border border-gray-300 px-4 py-2 text-left font-semibold text-white">LRN</th>
-          <th class="border border-gray-300 px-4 py-2 text-left font-semibold text-white">
-            Name of Learner
-          </th>
-          <th class="border border-gray-300 px-4 py-2 text-left font-semibold text-white">Sex</th>
-          <th class="border border-gray-300 px-4 py-2 text-left font-semibold text-white">
-            Birthday
-          </th>
-          <th class="border border-gray-300 px-4 py-2 text-left font-semibold text-white">
-            Date of Assessment
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td class="border border-gray-300 px-4 py-4"></td>
-          <td class="border border-gray-300 px-4 py-4"></td>
-          <td class="border border-gray-300 px-4 py-4"></td>
-          <td class="border border-gray-300 px-4 py-4"></td>
-          <td class="border border-gray-300 px-4 py-4"></td>
-          <td class="border border-gray-300 px-4 py-4"></td>
-        </tr>
-        <tr>
-          <td class="border border-gray-300 px-4 py-4"></td>
-          <td class="border border-gray-300 px-4 py-4"></td>
-          <td class="border border-gray-300 px-4 py-4"></td>
-          <td class="border border-gray-300 px-4 py-4"></td>
-          <td class="border border-gray-300 px-4 py-4"></td>
-          <td class="border border-gray-300 px-4 py-4"></td>
-        </tr>
-        <tr>
-          <td class="border border-gray-300 px-4 py-4"></td>
-          <td class="border border-gray-300 px-4 py-4"></td>
-          <td class="border border-gray-300 px-4 py-4"></td>
-          <td class="border border-gray-300 px-4 py-4"></td>
-          <td class="border border-gray-300 px-4 py-4"></td>
-          <td class="border border-gray-300 px-4 py-4"></td>
-        </tr>
-        <tr>
-          <td class="border border-gray-300 px-4 py-4"></td>
-          <td class="border border-gray-300 px-4 py-4"></td>
-          <td class="border border-gray-300 px-4 py-4"></td>
-          <td class="border border-gray-300 px-4 py-4"></td>
-          <td class="border border-gray-300 px-4 py-4"></td>
-          <td class="border border-gray-300 px-4 py-4"></td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="mt-6 overflow-x-auto">
+      <table class="w-full min-w-max border-collapse">
+        <thead class="bg-blue-600">
+          <tr>
+            <th class="border border-gray-300 px-3 py-2 text-left text-sm font-semibold text-white">
+              S/N
+            </th>
+            <th class="border border-gray-300 px-3 py-2 text-left text-sm font-semibold text-white">
+              LRN
+            </th>
+            <th class="border border-gray-300 px-3 py-2 text-left text-sm font-semibold text-white">
+              Name of Learner
+            </th>
+            <th class="border border-gray-300 px-3 py-2 text-left text-sm font-semibold text-white">
+              Section
+            </th>
+            <th class="border border-gray-300 px-3 py-2 text-left text-sm font-semibold text-white">
+              Sex
+            </th>
+            <th class="border border-gray-300 px-3 py-2 text-left text-sm font-semibold text-white">
+              Birthday
+            </th>
+            <th class="border border-gray-300 px-3 py-2 text-left text-sm font-semibold text-white">
+              Date of Assessment
+            </th>
+            <th
+              class="border border-gray-300 px-2 py-2 text-center text-sm font-semibold text-white"
+            >
+              Task A
+            </th>
+            <th
+              class="border border-gray-300 px-2 py-2 text-center text-sm font-semibold text-white"
+            >
+              Task B
+            </th>
+            <th
+              class="border border-gray-300 px-2 py-2 text-center text-sm font-semibold text-white"
+            >
+              Task C
+            </th>
+            <th
+              class="border border-gray-300 px-2 py-2 text-center text-sm font-semibold text-white"
+            >
+              Task D
+            </th>
+            <th
+              class="border border-gray-300 px-2 py-2 text-center text-sm font-semibold text-white"
+            >
+              Task E
+            </th>
+            <th
+              class="border border-gray-300 px-2 py-2 text-center text-sm font-semibold text-white"
+            >
+              Task F
+            </th>
+            <th
+              class="border border-gray-300 px-2 py-2 text-center text-sm font-semibold text-white"
+            >
+              Task G
+            </th>
+            <th
+              class="border border-gray-300 px-2 py-2 text-center text-sm font-semibold text-white"
+            >
+              Task H
+            </th>
+            <th
+              class="border border-gray-300 px-2 py-2 text-center text-sm font-semibold text-white"
+            >
+              Task I
+            </th>
+            <th
+              class="border border-gray-300 px-2 py-2 text-center text-sm font-semibold text-white"
+            >
+              Task J
+            </th>
+            <th
+              class="border border-gray-300 px-2 py-2 text-center text-sm font-semibold text-white"
+            >
+              Task K
+            </th>
+            <th
+              class="border border-gray-300 px-3 py-2 text-center text-sm font-semibold text-white"
+            >
+              Overall Score
+            </th>
+            <th
+              class="border border-gray-300 px-3 py-2 text-center text-sm font-semibold text-white"
+            >
+              Total Score
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="n in 10" :key="n" class="hover:bg-gray-50">
+            <td class="border border-gray-300 px-3 py-3 text-sm">{{ n }}</td>
+            <td class="border border-gray-300 px-3 py-3 text-sm"></td>
+            <td class="border border-gray-300 px-3 py-3 text-sm"></td>
+            <td class="border border-gray-300 px-3 py-3 text-sm"></td>
+            <td class="border border-gray-300 px-3 py-3 text-sm"></td>
+            <td class="border border-gray-300 px-3 py-3 text-sm"></td>
+            <td class="border border-gray-300 px-3 py-3 text-sm"></td>
+            <td class="border border-gray-300 px-2 py-3 text-center text-sm"></td>
+            <td class="border border-gray-300 px-2 py-3 text-center text-sm"></td>
+            <td class="border border-gray-300 px-2 py-3 text-center text-sm"></td>
+            <td class="border border-gray-300 px-2 py-3 text-center text-sm"></td>
+            <td class="border border-gray-300 px-2 py-3 text-center text-sm"></td>
+            <td class="border border-gray-300 px-2 py-3 text-center text-sm"></td>
+            <td class="border border-gray-300 px-2 py-3 text-center text-sm"></td>
+            <td class="border border-gray-300 px-2 py-3 text-center text-sm"></td>
+            <td class="border border-gray-300 px-2 py-3 text-center text-sm"></td>
+            <td class="border border-gray-300 px-2 py-3 text-center text-sm"></td>
+            <td class="border border-gray-300 px-2 py-3 text-center text-sm"></td>
+            <td class="border border-gray-300 px-3 py-3 text-center text-sm font-medium"></td>
+            <td class="border border-gray-300 px-3 py-3 text-center text-sm font-medium"></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </DashboardLayout>
 </template>
